beabfa03d56cc0bfb2a13a9a89e96db91541708b
ivloli <343900695@qq.com>
2026-04-25 11:42:43 +0800

    update jenkins(4)

--- jenkins/rollback_artifact_choices.groovy
+++ jenkins/rollback_artifact_choices.groovy
@@ -2,21 +2,21 @@ if (ACTION != 'Rollback') {
     return []
 }
 
-def cmd = $/
+def cmd = '''
 curl -s -u 'dnps:SXKvkwRICe743VPhTkKIbA==' \
   'https://artifactory.gainetics.io/artifactory/observe-dnps/prod-ipdb-xdb-manager/' \
-| perl -ne 'if (/href="([^"]+\.tar\.gz)".*?(\d{2})-([A-Za-z]{3})-(\d{4})\s+(\d{2}):(\d{2})/) { %m=(Jan=>1,Feb=>2,Mar=>3,Apr=>4,May=>5,Jun=>6,Jul=>7,Aug=>8,Sep=>9,Oct=>10,Nov=>11,Dec=>12); printf "%04d%02d%02d%02d%02d\t%s\n",$$4,$$m{$$3},$$2,$$5,$$6,$$1 }'
-| sort -r
+| grep -oE 'href="ipdb-manager-offline-linux-amd64-[^"]+\\.tar\\.gz"' \
+| sed -e 's/^href="//' -e 's/"$//' \
+| awk '!seen[$0]++' \
 | head -n 10
-| cut -f2
-/$
+'''
 
-def proc = ["bash", "-lc", cmd].execute()
+def proc = ['bash', '-lc', cmd].execute()
 proc.waitFor()
 
 if (proc.exitValue() != 0) {
-    return ["ERROR: failed to fetch artifacts"]
+    return ['ERROR: failed to fetch artifacts']
 }
 
 def lines = proc.in.text.readLines().findAll { it?.trim() }
-return lines ?: ["NO_ARTIFACT_FOUND"]
+return lines ?: ['NO_ARTIFACT_FOUND']
